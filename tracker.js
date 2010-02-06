edb9fb04e2211299093c9026e46006c4b4455034
Emil Hesslow <hesslow@guava.spotify.net.spotify.net>
2010-02-06 16:21:06 +0100

    Very simple implementations of addPeer and getPeers

--- tracker.js
+++ tracker.js
@@ -28,11 +28,13 @@ Peer.prototype = {
 
         c += String.fromCharCode(p >> 8);
         c += String.fromCharCode(p & 255);
+        return c;
     }
 }
 
 function File() {
-    this.peers = [];
+    this.peerList = [];
+    this.peers = {};
     this.downloads = 0;
     this.downloading = 0;
     this.uploading = 0;
@@ -40,10 +42,24 @@ function File() {
 
 File.prototype = {
     addPeer: function(peer) {
-        
+        if (typeof(this.peers[peer.id]) != "undefined") {
+            sys.puts("Update a old peer");
+            // Update the old peer object
+        } else {
+            sys.puts("Add a new peer");
+            this.peerList.push(peer);
+            this.peers[peer.id] = peer;
+        }
     },
-    getPeers: function() {
-        return "";
+    getPeers: function(count) {
+        var c = Math.min(this.peerList.length, count);
+        var ret = "";
+        for (var i = 0; i < c; i++) {
+            var index = Math.floor(Math.random() * this.peerList.length);
+            sys.puts("index: "+ index);
+            ret += this.peerList[index].compact_hostport;
+        }
+        return ret;
     }
 }
 
@@ -56,9 +72,11 @@ Tracker.prototype = {
         if (typeof this.files[info_hash] == "undefined")
             return this.addFile(info_hash);
 
+        sys.puts("Return old file");
         return this.files[info_hash];
     },
     addFile: function(info_hash) {
+        sys.puts("Create new file");
         var file = new File();
         this.files[info_hash] = file;
         return file;
@@ -78,6 +96,10 @@ function sendError(res, message) {
     res.finish();
 }
 
+// The requests that utorrent (and probably other) makes can't be urldecoded
+// using decodeURIComponent so decode the querystring using unescape and then
+// endocde it using encodeURIComponent. Which makes it possible for the
+// querystring lib to parse it.
 function fixUrlEncoding(url) {
     var newurl = "";
     for (var i = 0; i < url.length; i++) {
@@ -93,10 +115,8 @@ function fixUrlEncoding(url) {
 }
 
 var server = http.createServer(function (req, res) {
-    var urlstr = fixUrlEncoding(req.url);
-
-    params = url.parse(urlstr);
-    query = querystring.parse(params["query"], null, null, true);
+    var params = url.parse(req.url);
+    var query = querystring.parse(fixUrlEncoding(params["query"]), null, null, true);
 
     if (typeof query["info_hash"] == "undefined" ||
         typeof query["peer_id"] == "undefined" ||
@@ -109,7 +129,7 @@ var server = http.createServer(function (req, res) {
     var peer = new Peer(query["peer_id"], req.connection.remoteAddress, query["port"], query["left"]);
     var file = tracker.getFile(query["info_hash"]);
     file.addPeer(peer);
-    var peers = file.getPeers();
+    var peers = file.getPeers(10);
 
     var body = "d8:intervali10e5:peers"+ peers.length +":"+ peers +"e";
     res.sendHeader(200, {
